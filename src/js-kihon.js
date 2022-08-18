9000582c86b3059aedeac76543b9268d3be2f62b
MoritakaNishihira <111470183+MoritakaNishihira@users.noreply.github.com>
2022-08-18 10:29:21 +0900

    js-kihon アロー関数、分割代入

--- src/js-kihon.js
+++ src/js-kihon.js
@@ -1,12 +1,31 @@
 // テンプレート文字列
-const name = "moritaka";
-const age = "40";
+// const name = "moritaka";
+// const age = "40";
 
-const message = `わたしの名前は${name}です。年齢は${age}歳です。`;
-console.log(message);
+// const message = `わたしの名前は${name}です。年齢は${age}歳です。`;
+// console.log(message);
 
 // アロー関数
-const func1 = (str) => {
-  return str;
-};
-console.log(func1("allow"));
+// const func1 = (str) => str;
+// console.log(func1("allow"));
+
+// 分割代入（オブジェクト）
+// const myProfile = {
+//   name: "moritaka",
+//   age: 40
+// };
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+// 分割代入（配列）
+// const myProfile = ["moritaka", "40"];
+// const message1 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// console.log(message1);
+
+// const [name, age] = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
