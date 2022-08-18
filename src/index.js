49d654094abf63649e04197b89743740131de819
MoritakaNishihira <111470183+MoritakaNishihira@users.noreply.github.com>
2022-08-18 10:13:09 +0900

    update

--- src/index.js
+++
@@ -1,5 +0,0 @@
-const name = "moritaka";
-const age = "40";
-
-const message = `わたしの名前は${name}です。年齢は${age}歳です。`;
-console.log(message);
--- src/js-kihon.js
+++ src/js-kihon.js
@@ -4,3 +4,9 @@ const age = "40";
 
 const message = `わたしの名前は${name}です。年齢は${age}歳です。`;
 console.log(message);
+
+// アロー関数
+const func1 = (str) => {
+  return str;
+};
+console.log(func1("allow"));
